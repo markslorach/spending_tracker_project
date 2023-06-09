c325036505ecc5abdaf971a13fd58a2dd6aaf664
Mark Slorach <hello@markslorach.com>
2023-06-09 14:32:23 +0100

    test_merchant_has_name - passed

--- tests/merchant_tests.py
+++ tests/merchant_tests.py
@@ -2,4 +2,9 @@ import unittest
 from models.merchant import Merchant
 
 class TestMerchant(unittest.TestCase):
-    pass
+
+    def setUp(self):
+        self.merchant = Merchant("Falafel To Go")
+
+    def test_merchant_has_name(self):
+        self.assertEqual("Falafel To Go", self.merchant.name)
