399158a0e15c7fa4493e1cd07bc418176c965768
Mark Slorach <hello@markslorach.com>
2023-06-09 14:51:38 +0100

    temporarily removed date from transaction class

--- models/transaction.py
+++ models/transaction.py
@@ -1,8 +1,7 @@
 class Transaction:
     # possible additions - date, time, user
-    def __init__(self, amount, date, merchant, tag, id = None):
+    def __init__(self, amount, merchant, tag, id = None):
         self.amount = amount
-        self.date = date
         self.merchant = merchant
         self.tag = tag
         self.id = id
