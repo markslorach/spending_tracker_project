0a6749dcbee625e6a8a1062ac7110c18094e425d
Mark Slorach <hello@markslorach.com>
2023-06-09 17:14:58 +0100

    added imports to console.py

---
+++ console.py
@@ -0,0 +1,5 @@
+import pdb
+from models.merchant import Merchant
+from models.transaction import Transaction
+import repositories.merchant_repository as merchant_repository
+import repositories.transaction_repository as transaction_repository
