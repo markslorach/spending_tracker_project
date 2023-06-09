ff35510792f48991ba0c4a098ab90f0b5bd1f382
Mark Slorach <hello@markslorach.com>
2023-06-09 14:49:13 +0100

    test_transaction_has_amount - passed

--- tests/transaction_tests.py
+++ tests/transaction_tests.py
@@ -2,4 +2,9 @@ import unittest
 from models.transaction import Transaction
 
 class TestTransaction(unittest.TestCase):
-    pass
+    
+    def setUp(self):
+        self.transaction = Transaction(4.50, "Falafel To Go", "Takeaway")
+    
+    def test_transaction_has_amount(self):
+        self.assertEqual(4.50, self.transaction.amount)
