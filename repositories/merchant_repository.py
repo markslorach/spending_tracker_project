de8e994a23dfa241b45b4b412243a05a2eb37129
Mark Slorach <hello@markslorach.com>
2023-06-09 18:50:00 +0100

    added select all function

--- repositories/merchant_repository.py
+++ repositories/merchant_repository.py
@@ -3,7 +3,7 @@ from db.run_sql import run_sql
 from models.merchant import Merchant
 from models.transaction import Transaction
 
-# Saves a merchant to the database
+# Saves merchant to the database
 def save(merchant):
     sql = "INSERT INTO merchants (name) VALUES (%s) RETURNING *"
     values = [merchant.name]
@@ -12,4 +12,12 @@ def save(merchant):
     merchant.id = id
     return merchant
     
-    
+# Selects all merchants from the database
+def select_all():
+    merchants = []
+    sql = "SELECT * FROM merchants"
+    results = run_sql(sql)
+    for row in results:
+        merchant = Merchant(row['name'], row['id'])
+        merchants.append(merchant)
+    return merchants
